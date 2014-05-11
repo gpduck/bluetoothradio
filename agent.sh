74eb0d1e79c571df95487c564ca61a0d04cf9531
Chris Duck <gpduck@whatsupduck.net>
2014-05-11 12:42:02 -0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 # Load a bluetooth agent to allow the pairing and connection, and to
 # Set pulseaudio up against the bluetooth connection.
-bluetooth-agent $(cat bluetoothPin)
+DIR=$( readlink -f "$( dirname "$0" )")
+bluetooth-agent $(cat $DIR/bluetoothPin)
