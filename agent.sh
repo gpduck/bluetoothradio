1887b7e3e2c83ff51c79d2ba8aea52fc5794be73
Chris Duck <gpduck@whatsupduck.net>
2014-05-11 14:11:00 -0500

    bring up bluetooth network

--- agent.sh
+++ agent.sh
@@ -2,4 +2,5 @@
 # Load a bluetooth agent to allow the pairing and connection, and to
 # Set pulseaudio up against the bluetooth connection.
 DIR=$( readlink -f "$( dirname "$0" )")
+hciconfig hci0 up
 bluetooth-agent $(cat $DIR/bluetoothPin)
