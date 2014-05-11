c9ad74f7f93024b3dc09402f811a81a8eb7d7260
Chris Duck <gpduck@whatsupduck.net>
2014-05-11 02:39:54 -0500

    reduce logind noise when we su in our loops

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /et
 
 sudo git clone https://github.com/gpduck/bluetoothradio /root/bluetoothradio
 sudo cp /root/bluetoothradio/bluetooth-server.service /etc/systemd/system
+sudo cp /root/bluetoothradio/systemd-logind.service /etc/systemd/system
 sudo systemctl enable bluetooth-server.service
