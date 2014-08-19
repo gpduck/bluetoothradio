d446bb4d7a8f87f7f9db0a4171b54e644a231ad0
Chris Duck <gpduck@whatsupduck.net>
2014-08-19 01:36:49 -0500

    fix typo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /et
 sudo git clone https://github.com/gpduck/bluetoothradio /root/bluetoothradio
 sudo cp /root/bluetoothradio/bluetooth-server.service /etc/systemd/system
 sudo cp /root/bluetoothradio/systemd-logind.service /etc/systemd/system
-suco cp /root/bluetoothradio/pulseaudio.service /etc/systemd/system
+sudo cp /root/bluetoothradio/pulseaudio.service /etc/systemd/system
 sudo systemctl enable bluetooth-server.service
 sudo systemctl enable pulseaudio.service
 sudo update-rc.d pulseaudio disable
