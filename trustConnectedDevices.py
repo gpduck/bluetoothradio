b7feeba8e1570ed8780470741e22321165f647a8
Chris Duck <gpduck@whatsupduck.net>
2014-08-19 23:49:31 -0500

    removed event loop stuff

--- trustConnectedDevices.py
+++ trustConnectedDevices.py
@@ -4,14 +4,7 @@ import gobject
 
 import sys
 import dbus
-from dbus.mainloop.glib import DBusGMainLoop
 
-DBusGMainLoop(set_as_default=True)
-
-loop = gobject.MainLoop()
-loop.run()
-
-dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SystemBus()
 manager = dbus.Interface(bus.get_object("org.bluez", "/"), "org.bluez.Manager")
 
