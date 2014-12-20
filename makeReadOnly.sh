21df69e31fdbbadc98c183d923da499da804dabe
Chris Duck <gpduck@whatsupduck.net>
2014-12-20 15:24:53 -0600

    Update makeReadOnly.sh

--- makeReadOnly.sh
+++ makeReadOnly.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+#disable swap
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile disable
+
 #Configure unionfs
 sudo apt-get install unionfs-fuse
 
 cat << EOF > /tmp/mount_unionfs
 #!/bin/sh
-DIR=$1
+DIR=$$1
 
 
-if [ ! -e /tempfs${DIR} ]
+if [ ! -e /tempfs$${DIR} ]
 then
-  mkdir /tempfs${DIR}
+  mkdir /tempfs$${DIR}
 fi
 
-if [ ! -e ${DIR} ]
+if [ ! -e $${DIR} ]
 then
-  mkdir ${DIR}
+  mkdir $${DIR}
 fi
 
 /usr/bin/unionfs-fuse -o cow,allow_other,nonempty /tempfs${DIR}=RW:${DIR}_ro=RO ${DIR}
@@ -45,11 +50,11 @@ sudo chmod --reference=/tmp_ro /tmp
 #mark / as read-only
 awk '$2~"^/$"&&$4!~"ro"{$4=$4",ro"}1' OFS="\t" /etc/fstab > /tmp/fstab
  
-echo -e "tmpfs\t/tempfs\ttmpfs\tdefaults,size=30M\t0\t0" >> /tmp/fstab
+echo "tmpfs\t/tempfs\ttmpfs\tdefaults,size=30M\t0\t0" >> /tmp/fstab
  
-echo -e "mount_unionfs\t/etc\tfuse\tdefaults\t0\t0" >> /tmp/fstab
-echo -e "mount_unionfs\t/var\tfuse\tdefaults\t0\t0" >> /tmp/fstab
-echo -e "mount_unionfs\t/home\tfuse\tdefaults\t0\t0" >> /tmp/fstab
-echo -e "mount_unionfs\t/root\tfuse\tdefaults\t0\t0" >> /tmp/fstab
-echo -e "mount_unionfs\t/tmp\tfuse\tdefaults\t0\t0" >> /tmp/fstab
+echo "mount_unionfs\t/etc\tfuse\tdefaults\t0\t0" >> /tmp/fstab
+echo "mount_unionfs\t/var\tfuse\tdefaults\t0\t0" >> /tmp/fstab
+echo "mount_unionfs\t/home\tfuse\tdefaults\t0\t0" >> /tmp/fstab
+echo "mount_unionfs\t/root\tfuse\tdefaults\t0\t0" >> /tmp/fstab
+echo "mount_unionfs\t/tmp\tfuse\tdefaults\t0\t0" >> /tmp/fstab
 sudo mv /tmp/fstab /etc/fstab
