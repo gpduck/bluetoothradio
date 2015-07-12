8e37ca062743c178d2d83774392642be7f45794f
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 13:20:11 -0500

    fixed to correct path

--- agent.sh
+++ agent.sh
@@ -2,4 +2,4 @@
 # Load a bluetooth agent to allow the pairing and connection, and to
 # Set pulseaudio up against the bluetooth connection.
 hciconfig hci0 up
-bt-agent -p /etc/bluetooth/bluetoothPin -c NoInputNoOutput
+bt-agent -p /etc/bluetoothradio/bluetoothPin -c NoInputNoOutput
