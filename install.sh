8d3d12dd940e74eb5f4899103dcd0182e6e6d5a6
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 13:04:46 -0500

    dont' assume clone to /root/

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ sudo usermod -a -G lp pi
 sudo sed -i '/\[General\]/c\\[General\]\nEnable=Source,Sink,Media,Socket' /etc/bluetooth/audio.conf
 sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /etc/pulse/daemon.conf
 
-sudo cp /root/bluetoothradio/bluetooth-server.service /etc/systemd/system
-sudo cp /root/bluetoothradio/systemd-logind.service /etc/systemd/system
-sudo cp /root/bluetoothradio/pulseaudio.service /etc/systemd/system
-sudo cp /root/bluetoothradio/bluetooth-agent.service /etc/systemd/system
+sudo cp bluetooth-server.service /etc/systemd/system
+sudo cp systemd-logind.service /etc/systemd/system
+sudo cp pulseaudio.service /etc/systemd/system
+sudo cp bluetooth-agent.service /etc/systemd/system
 sudo systemctl enable bluetooth-server.service
 sudo systemctl enable pulseaudio.service
 sudo update-rc.d pulseaudio disable
