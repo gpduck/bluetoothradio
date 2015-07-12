cdd330015ceeaf587a5f80f09e29e937498ad107
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 13:00:38 -0500

    Update to use bt-agent

--- agent.sh
+++ agent.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Load a bluetooth agent to allow the pairing and connection, and to
 # Set pulseaudio up against the bluetooth connection.
-DIR=$( readlink -f "$( dirname "$0" )")
 hciconfig hci0 up
-bluetooth-agent $(cat $DIR/bluetoothPin)
+bt-agent -p /etc/bluetooth/bluetoothPin -c NoInputNoOutput
