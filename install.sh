c0d5e826333345af8b7574d6bc46ef51b1805b71
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 18:46:03 -0500

    policy allows all this to happen automatically

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ sudo adduser pulse lp
 sudo sed -i '/\[General\]/c\\[General\]\nEnable=Source,Sink,Media,Socket' /etc/bluetooth/audio.conf
 sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /etc/pulse/daemon.conf
 
-sudo mkdir /usr/lib/bluetoothradio
+cp /etc/pulse/system.pa /tmp/system.pa
+cat << EOF > /tmp/system.pa
 
-sudo cp connect.sh /usr/lib/bluetoothradio/
-sudo cp startup.wav /usr/lib/bluetoothradio/
-sudo cp bluetooth-server.service /etc/systemd/system
+.ifexists module-bluetooth-discover.so
+  load-module module-bluetooth-discover
+.endif
+
+.ifexists module-bluetooth-policy.so
+  load-module module-bluetooth-policy
+.endif
+EOF
+sudo mv /tmp/system.pa /etc/pulse/system.pa
 
 sudo cp systemd-logind.service /etc/systemd/system
 
 sudo cp pulseaudio.service /etc/systemd/system
 
-sudo mkdir /etc/bluetoothradio
-sudo cp bluetoothPin /etc/bluetoothradio/
-sudo cp agent.sh /usr/lib/bluetoothradio/
-sudo cp bluetooth-agent.service /etc/systemd/system
-
-sudo systemctl enable bluetooth-server.service
 sudo systemctl enable pulseaudio.service
-sudo update-rc.d pulseaudio disable
