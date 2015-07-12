5e8d0786a127e5433da4b98d5ef4a34a07366a4b
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 13:04:12 -0500

    will 'git clone' outside of this file.

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ sudo usermod -a -G lp pi
 sudo sed -i '/\[General\]/c\\[General\]\nEnable=Source,Sink,Media,Socket' /etc/bluetooth/audio.conf
 sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /etc/pulse/daemon.conf
 
-sudo git clone https://github.com/gpduck/bluetoothradio /root/bluetoothradio
 sudo cp /root/bluetoothradio/bluetooth-server.service /etc/systemd/system
 sudo cp /root/bluetoothradio/systemd-logind.service /etc/systemd/system
 sudo cp /root/bluetoothradio/pulseaudio.service /etc/systemd/system
