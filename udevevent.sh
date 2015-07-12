a44861cccb994cadf95e894debb4f7877c7d1f18
Chris Duck <gpduck@whatsupduck.net>
2015-07-12 18:15:58 -0500

    bugfixes

--- udevevent.sh
+++ udevevent.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-hci="$(echo $0 | grep -o "hci[0-9]\+")"
+hci="$(echo $1 | grep -o "hci[0-9]\+")"
 dbus="$(qdbus --system org.bluez | grep -m 1 "${hci}/dev_")"
 mac="$(echo $dbus | rev | cut -c -17 | rev)"
 bluezSource="bluez_source.${mac}"
-logger --tag bluetoothradio "${ACTION} event for ${bluezSource}"
+logger --tag bluetoothradio "$ACTION event for ${bluezSource}"
 case "$ACTION" in
 	add)
-		pactl "set-sink-mute 0 1"
-
 		pactl "set-sink-mute 0 0"
-		pactl set-sink-volume 0 100%
+		pactl set-sink-volume 0 80%
 		
 		# Return Example: alsa_output.pci-0000_00_10.1.analog-stereo
 		alsaSink="$(pactl list | grep -m 1 "Name: alsa_output" | cut -c 8-)"
