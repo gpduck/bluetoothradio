495b53dece7c71c440b501ae828fd0cb118e28a4
Chris Duck <gpduck@whatsupduck.net>
2015-07-13 02:09:16 -0500

    need to run an agent

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install bluez bluez-tools pulseaudio-module-bluetooth python-gobject python-gobject-2 -y
 sudo adduser pulse audio
 sudo adduser pulse lp
+sudo adduser root pulse-access
 
 sudo sed -i '/\[General\]/c\\[General\]\nEnable=Source,Sink,Media,Socket' /etc/bluetooth/audio.conf
 sudo sed -i '/; resample-method = speex-float-3/c\resample-method = trivial' /etc/pulse/daemon.conf
@@ -18,8 +19,16 @@ cat << EOF >> /tmp/system.pa
 EOF
 sudo mv /tmp/system.pa /etc/pulse/system.pa
 
+sudo mkdir /usr/lib/bluetoothradio
+sudo cp agent.sh /usr/lib/bluetoothradio/
+sudo mkdir /etc/bluetoothradio
+sudo cp bluetoothPin /etc/bluetoothradio/
+sudo cp bluetooth-agent.service /etc/systemd/system
+
+
 sudo cp systemd-logind.service /etc/systemd/system
 
 sudo cp pulseaudio.service /etc/systemd/system
 
 sudo systemctl enable pulseaudio.service
+sudo systemctl enable bluetooth-agent.service
